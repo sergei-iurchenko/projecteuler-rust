257f8c3c6eea8448e478cb9cd3c1027f68a5148a
Urchenko <urchenko88@gmail.com>
2015-02-21 02:15:17 +0300

    357

--- 357/src/main.rs
+++ 357/src/main.rs
@@ -2,33 +2,32 @@ extern crate time;
 
 use std::num::{Float};
 use time::precise_time_s;
-use std::collections::bitv;
-use std::collections::HashSet;
+use std::collections::{BitVec, BitSet};
 use std::iter;
 
-fn get_primes_below(n: usize) -> Vec<usize> {
-    let mut sieve = bitv::Bitv::from_elem(n, true);
+fn get_primes_below(n: usize)-> BitSet {
+    let mut sieve = BitVec::from_elem(n, true);
     let stop = sieve.len();
-    for i in iter::range_step(3, (n as f64).sqrt() as usize + 1us, 2) {
+    for i in iter::range_step(3, (n as f64).sqrt() as usize + 1, 2) {
         if sieve[i] == true {
             for j in iter::range_step(i * i, stop, 2 * i) {
                 sieve.set(j, false);
             }
         }
     }
-
-    let mut result:Vec<usize> = Vec::with_capacity(n);
-    result.push(2);
+    let mut result = Box::new(BitSet::with_capacity(n/15));
+    result.insert(2);
     for i in iter::range_step(3, n, 2) {
         if sieve[i] == true {
-            result.push(i)
+            result.insert(i);
         };
     }
-    result
+    println!("{}", result.len());
+    *result
 }
 
-fn check(n: usize, primes: &HashSet<usize>) -> bool {
-    if ![0us, 2, 8].contains(&(n % 10))
+fn check(n: usize, primes: &BitSet) -> bool {
+    if ![0usize, 2, 8].contains(&(n % 10))
     {
         return false;
     }
@@ -47,17 +46,13 @@ fn check(n: usize, primes: &HashSet<usize>) -> bool {
     return true;
 }
 
-
-fn main() {
+fn find(n: usize) -> usize {
     let start_time = precise_time_s();
-    let a = 100000000us;
-    let primes_vec = get_primes_below(a);
+    let primes = Box::new(get_primes_below(n));
     println!("{} sec.", precise_time_s() - start_time);
-    let primes: HashSet<usize> = primes_vec.iter().map(|&x| x).collect();
-    println!("{} sec.", precise_time_s() - start_time);
-    let mut sum = 7us;
+    let mut sum = 7usize;
     for x in primes.iter() {
-        let _x = *x - 1us;
+        let _x = x - 1;
         if check(_x, &primes) == true {
             sum += _x;
         }
@@ -65,5 +60,17 @@ fn main() {
 
     println!("{} sec.", precise_time_s() - start_time);
     println!("answer = {}", sum);
+    sum
+}
+
+fn main() {
+    let n = 1000_000_00usize;
+    find(n);
+}
+
+#[test]
+fn it_works() {
+    let n = 1000_000_00usize;
+    assert!(find(n) == 1739023853137usize);
 }
 
