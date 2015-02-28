afcb31e201f61ed5ec7fea6d68c01eb9d79756cf
Urchenko <amitivil@list.ru>
2015-02-28 23:23:18 +0300

    Доделал 504

--- 504/src/main.rs
+++ 504/src/main.rs
@@ -1,59 +1,60 @@
-use std::num::{SignedInt, Float};
+extern crate time;
 
-fn is_square_number(number: isize) -> bool {
-    let number_precise = (number as f32).sqrt();
-    let number_rounded = number_precise.trunc();
-    if number_precise - number_rounded == Float::zero() {true} else {false}
+use std::collections::BitSet;
+
+use time::precise_time_s;
+
+fn gcd(a: usize, b: usize) -> usize {
+        if b == 0usize {
+            a
+        } else {
+            gcd(b, a % b)
+        }
 }
 
-fn get_y(y: f32) -> isize {
-    if y == Float::zero() {return 0}
-    let _y = y.abs();
-    let y_floor = _y.trunc();
-    if _y - y_floor == Float::zero() {
-        y_floor as isize - 1
-    }
-    else {
-      _y as isize
-    }
+fn calc_sum(a: usize, b: usize, c: usize, d: usize) -> usize {
+    let CB = gcd(c,b);
+    let AB = gcd(a,b);
+    let DA = gcd(d,a);
+    let CD = gcd(c,d);
+    let G = CB + AB + DA + CD;
+    let square = a*b + b*c + c*d + d*a;
+    let B2 = square - G + 2;
+//    if B2 < 0 {
+//        println!("square-{}", square);
+//        println!("G-{}", G);
+//        println!("{}-{}", B2, B2/2);
+//        println!("{} {} {} {}", a, b, c, d);
+//        println!("{} {} {} {}", CB, AB, DA, CD);
+//        panic!("B2<0");
+//    }
+//    if B2 % 2 != 0 {
+//        println!("square-{}", square);
+//        println!("G-{}", G);
+//        println!("{}-{}", B2, B2/2);
+//        println!("{} {} {} {}", a, b, c, d);
+//        println!("{} {} {} {}", CB, AB, DA, CD);
+//        panic!("B2 % 2 != 0");
+//    }
+    
+    B2 / 2
 }
 
-fn calc_sum(a: isize, b: isize, c: isize, d: isize) -> isize {
-    let mut sum = a + b + c + d - 3;
-    for x in (-c + 1)..0 {
-        let y = ((b as f32) / (c as f32) * (x as f32)) + (b as f32);
-        sum += get_y(y);        
-//        println!("a={}, b={}, c={}, d={}", a, b, c, d);
-//        println!("x={}, y={}, y_pres={}", x, y, get_y(y));
+fn cycle_simple(n: usize) -> usize {
+    let mut powers = BitSet::new();
+    for i in 1..2*n+1 {
+        powers.insert((i*i) as usize);
     }
-    for x in 1..a {
-        let y = (-(b as f32) / (a as f32) * (x as f32)) + (b as f32);
-        sum += get_y(y);        
-    }
-    for x in (-c + 1)..0 {
-        let y = (-(d as f32) / (c as f32) * (x as f32)) - (d as f32);
-        sum += get_y(y);        
-    }
-    for x in 1..a {
-        let y = ((d as f32) / (a as f32) * (x as f32)) - (d as f32);
-        sum += get_y(y);        
-    }
-    sum
-}
 
-fn cycle_simple(n: isize) -> isize {
-    let powers = (1..(2 * n + 1)).map(|x| x*x).collect::<Vec<isize>>();
-    println!("{}", powers.contains(*(25isize)));
     let m = n + 1;
-    let mut quantity = 0isize;
+    let mut quantity = 0usize;
     for a in 1..m {
         for b in 1..m {
             for c in 1..m {
                 for d in 1..m {
                     let result = calc_sum(a, b, c, d);
-                    if is_square_number(result) {
-//                        println!("{}", result);
-                        quantity += 1
+                    if powers.contains(&result) {
+                        quantity += 1;
                     };
                 }
             }
@@ -63,19 +64,11 @@ fn cycle_simple(n: isize) -> isize {
 }
 
 fn main() {
-    let m = 31;
-    let result = cycle_simple(m);
-    println!("Итого при m={}: {}", m, result);
+    let start_time = precise_time_s();
+    for i in 100..101 {
+        let result = cycle_simple(i);
+        println!("Итого при m={}: {}", i, result);
+    }
+    println!("{} sec.", precise_time_s() - start_time);
 }
 
-#[test]
-fn test_get_y() {
-    assert!(get_y(5f32) == 4isize);
-    assert!(get_y(5.1f32) == 5isize);
-    assert!(get_y(-5.1f32) == 5isize);
-    assert!(get_y(-1.1f32) == 1isize);
-    assert!(get_y(-0.9f32) == 0isize);
-    assert!(get_y(0.9f32) == 0isize);
-    assert!(get_y(0.0f32) == 0isize);
-    assert!(get_y(1.0f32) == 0isize);
-}
