dcdc11c8d688506e64312c117d12e5c9d8c3a052
Sergey Yurchenko <urchenko88@gmail.com>
2016-12-24 10:47:50 +0300

    357 works but very slowly

--- 357/src/main.rs
+++ 357/src/main.rs
@@ -1,19 +1,19 @@
 extern crate time;
+extern crate bit_vec;
+
 
 use time::precise_time_s;
 use std::collections::BTreeSet;
+use bit_vec::BitVec;
 
 fn get_primes_below(max_digit: usize)-> BTreeSet<usize> {
-    let mut sieve: Vec<bool> = Vec::with_capacity(max_digit);
-    for _ in 0..max_digit {
-        sieve.push(true);
-    }
+    let mut sieve = BitVec::from_elem(max_digit, true);
     let stop = sieve.len();
     let top = (max_digit as f64).sqrt() as usize + 1;
     for (_, i) in (3..top).enumerate().filter(|&(index, _)| index % 2 == 0) {
         if sieve[i] == true {
-            for (_, j) in (i*i..stop).enumerate().filter(|&(index, _)| index % (2 * i) == 0 ) {
-                sieve[j] = false;
+            for (_, j) in ((i*i)..stop).enumerate().filter(|&(index, _)| index % (2 * i) == 0 ) {
+                sieve.set(j, false);
             }
         }
     }
@@ -67,18 +67,11 @@ fn main() {
     let n = 1000_000_00usize;
     let result = find(n);
     println!("answer = {}", result);
-//    for (index, value) in (3..20).enumerate().filter(|&(index, _)| index % 2 == 0) {
-//        println!("{}", value)
-//    }
 }
+
 
 #[test]
 fn it_works() {
     let n = 1000_000_00usize;
     assert!(find(n) == 1739023853137usize);
 }
-
-//#[bench]
-//fn bench_it(b: &mut Bencher) {
-//    b.iter(|| find(1000));
-//}
